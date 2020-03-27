8d12639ade205da3fd34e1f47b35c0c42f525cfc
Michał Krzysztof Feiler <m@mikf.pl>
2020-03-27 18:29:10 +0100

    added /supl

--- configuration.nix
+++ configuration.nix
@@ -193,4 +193,10 @@ in
    device = "/dev/disk/by-uuid/ad19d261-fa12-43d1-9e9e-c733ebb67440";
    options = [ "noauto" "users" "ro" "noexec" "nodev" "nosuid" "async" ];
  };
+
+ fileSystems."/supl" = {
+   fsType = "f2fs";
+   device = "/dev/disk/by-uuid/0e20551a-075f-4485-a465-f6ff743347dc";
+ };
+ 
 }
