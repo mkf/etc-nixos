39adedd182e913b8be1b2c0fd66fa547ba9bd741
Michał Krzysztof Feiler <m@mikf.pl>
2020-10-30 13:18:16 +0100

    filesystem /chudy

--- configuration.nix
+++ configuration.nix
@@ -250,6 +250,12 @@ in
   # should.
   system.stateVersion = "19.03"; # Did you read the comment?
 
+  fileSystems."/chudy" = {
+    fsType = "ext4";
+    device = "/dev/disk/by-uuid/6cb49a46-e8f6-47fd-af3e-c1a4f043fe68";
+    options = [ "noauto" "users" "rw" "noexec" "nodev" "nosuid" "async" ];
+  };
+  
   fileSystems."/archhome" = {
     fsType = "xfs";
     device = "/dev/disk/by-uuid/ad19d261-fa12-43d1-9e9e-c733ebb67440";
