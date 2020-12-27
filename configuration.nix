a95efaf14f01015bfc0af0c8b4a6733e89ccfbc4
Michał Krzysztof Feiler <m@mikf.pl>
2020-12-27 23:27:42 +0100

    unpermit insecure openssl-1.0.2u and p7zip-16.02

--- configuration.nix
+++ configuration.nix
@@ -127,10 +127,6 @@
   ];
 
   nixpkgs.config.allowUnfree = true;
-  nixpkgs.config.permittedInsecurePackages = [
-    "openssl-1.0.2u"
-    "p7zip-16.02"
-  ];
 
   hardware.opengl.driSupport32Bit = true;
 
