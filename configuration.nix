c9323104620340fc86320347e0a8ea60e541e141
Michał Krzysztof Feiler <m@mikf.pl>
2021-04-24 12:47:41 +0200

    hardware.bluetooth settings→config, since c835f5f3dcf928ed6a0f5583780fea3f11ea92ae cherry-picked from fec7516bbf479bb56243d17009126913c3e353de was not enough
    (cherry picked from commit b172ff5d0b82e7189026ecff582ff5a117efaac3)

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,7 @@
 
   hardware.bluetooth = {
     enable = true;
-    settings.General.Enable = "Source,Sink,Media,Socket";
+    config.General.Enable = "Source,Sink,Media,Socket";
   };
   services.blueman.enable = true;
 
