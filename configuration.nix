b760319558fe2258a8fd97ceb9568118ca899951
Mika Feiler <m@mikf.pl>
2022-01-21 02:16:47 +0100

    Revert "hardware.bluetooth settings→config, since c835f5f3dcf928ed6a0f5583780fea3f11ea92ae cherry-picked from fec7516bbf479bb56243d17009126913c3e353de was not enough"
    This reverts commit b172ff5d0b82e7189026ecff582ff5a117efaac3.

--- configuration.nix
+++ configuration.nix
@@ -77,7 +77,7 @@
 
   hardware.bluetooth = {
     enable = true;
-    config.General.Enable = "Source,Sink,Media,Socket";
+    settings.General.Enable = "Source,Sink,Media,Socket";
   };
   services.blueman.enable = true;
 
