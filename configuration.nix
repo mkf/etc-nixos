e2d189d6320f564914d0b4cf948f0d67a76e6610
Mika Feiler <m@mikf.pl>
2023-01-14 10:51:04 +0100

    grub/Haiku,beforeNixOS

--- configuration.nix
+++ configuration.nix
@@ -14,6 +14,13 @@
   boot.loader.grub.version = 2;
   boot.loader.grub.device = "/dev/disk/by-id/ata-TOSHIBA_MK8009GAH_3799WGE8W";
   boot.loader.grub.useOSProber = true;
+  boot.loader.grub.extraEntries = ''
+    menuentry "Haiku" {
+      set root=(hd0,3);
+      chainloader +1
+    }
+  '';
+  boot.loader.grub.extraEntriesBeforeNixOS = true;
 
   networking.hostName = "szumik"; # Define your hostname.
   networking.wireless.enable = true;  # Enables wireless support via wpa_supplicant.
